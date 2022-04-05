66adbd72e734535ec53cc953cbfc70fc648cd414
Lothar Wendehals <lothar.wendehals@itemis.de>
2022-04-06 00:01:03 +0200

    version incremented

--- cmd/root.go
+++ cmd/root.go
@@ -11,13 +11,13 @@ import (
 var RootCmd = &cobra.Command{
 	Use:   "bricks",
 	Short: "A command line interface to Rebrickable and bricks collections",
-	Long: `bricks is a command line interface to the Rebrickable.com API. It
+	Long: `bricks is a command line interface to the rebrickable.com API. It
 enables you to access the Rebrickable database to retrieve for example a list
 of all bricks of a certain set or user specific brick collections.
 
 bricks is also able to merge, sort, add, or subtract collections of bricks
 to new collections.`,
-	Version: "0.1.0",
+	Version: "0.2.0",
 
 	CompletionOptions: cobra.CompletionOptions{
 		DisableDefaultCmd: true},

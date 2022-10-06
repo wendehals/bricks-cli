b27d52294b01f7e33df23cd54d96023f237e26b8
Lothar Wendehals <lothar.wendehals@itemis.de>
2022-10-06 15:51:31 +0200

    Refactored build

--- build/Build.go
+++ build/Build.go
@@ -16,10 +16,21 @@ const (
 )
 
 func Build(neededCollection *model.Collection, providedCollection *model.Collection, mode uint8, outputDir string, verbose bool) {
-	colors := model.GetColors(false)
-	partRelationships := model.GetPartRelationships(false)
+	buildCollection := &model.BuildCollection{}
+	if len(neededCollection.Sets) > 0 {
+		buildCollection.Set = neededCollection.Sets[0]
+	}
+	buildCollection.Mapping = []model.PartMapping{}
 
-	buildCollection := build(neededCollection, providedCollection, colors, partRelationships, mode)
+	// filter spare parts
+	neededCollection.Filter(func(p model.Part) bool {
+		return !p.IsSpare
+	})
+
+	mapSameShapeSameColor(neededCollection, providedCollection, buildCollection)
+	mapEquivalentShapeSameColor(providedCollection, buildCollection, mode)
+	mapEquivalentShapeDifferentColor(providedCollection, buildCollection, mode)
+
 	buildCollection.Sort()
 
 	model.Save(buildCollection, fmt.Sprintf("%s/result.build", outputDir))
@@ -30,90 +41,55 @@ func Build(neededCollection *model.Collection, providedCollection *model.Collect
 	export.ExportCollectionToHTML(providedCollection, outputDir, "remaining")
 }
 
-func build(neededCollection *model.Collection, providedCollection *model.Collection, colors *model.Colors,
-	partRelationships *model.PartRelationships, mode uint8) *model.BuildCollection {
-
-	buildCollection := model.BuildCollection{}
-	if len(neededCollection.Sets) > 0 {
-		buildCollection.Set = neededCollection.Sets[0]
-	}
-	buildCollection.Mapping = []model.PartMapping{}
-
-	isMold := func(s1, s2 string) bool {
-		return partRelationships.IsMold(s1, s2)
-	}
-
-	isPrint := func(s1, s2 string) bool {
-		return partRelationships.IsPrint(s1, s2)
-	}
-
-	isAlternative := func(s1, s2 string) bool {
-		return partRelationships.IsAlternative(s1, s2)
-	}
-
-	// filter spare parts
-	neededCollection.Filter(func(p model.Part) bool {
-		return !p.IsSpare
-	})
-
-	mapSameShapeSameColor(neededCollection, providedCollection, buildCollection)
-	mapEquivalentShapeSameColor(mode, buildCollection, providedCollection, isMold, isPrint, isAlternative)
-	mapEquivalentShapeDifferentColor(mode, buildCollection, colors, providedCollection, isMold, isPrint, isAlternative)
-
-	return &buildCollection
-}
-
-func mapSameShapeSameColor(neededCollection *model.Collection, providedCollection *model.Collection, buildCollection model.BuildCollection) {
+func mapSameShapeSameColor(neededCollection *model.Collection, providedCollection *model.Collection, buildCollection *model.BuildCollection) {
 	for _, neededPart := range neededCollection.Parts {
 		mapping := model.PartMapping{}
 		mapping.Quantity = neededPart.Quantity
 		mapping.Original = *model.DeepClone(&neededPart, &model.Part{})
 		mapping.Substitutes = []model.Part{}
 
-		mapPart(&mapping, providedCollection, neededPart.Color.ID, utils.Equals)
+		mapPart(providedCollection, &mapping, neededPart.Color.ID, utils.Equals)
 
 		buildCollection.Mapping = append(buildCollection.Mapping, mapping)
 	}
 }
 
-func mapEquivalentShapeSameColor(mode uint8, buildCollection model.BuildCollection, providedCollection *model.Collection,
-	isMold func(s1 string, s2 string) bool, isPrint func(s1 string, s2 string) bool, isAlternative func(s1 string, s2 string) bool) {
-
+func mapEquivalentShapeSameColor(providedCollection *model.Collection, buildCollection *model.BuildCollection, mode uint8) {
 	if mode&MOLDS != 0 || mode&PRINTS != 0 || mode&ALTERNATES != 0 {
 		for i := range buildCollection.Mapping {
 			mapping := &buildCollection.Mapping[i]
 
 			if mode&MOLDS != 0 {
-				mapPart(mapping, providedCollection, mapping.Original.Color.ID, isMold)
+				mapPart(providedCollection, mapping, mapping.Original.Color.ID, isMold)
 			}
 			if mode&PRINTS != 0 {
-				mapPart(mapping, providedCollection, mapping.Original.Color.ID, isPrint)
+				mapPart(providedCollection, mapping, mapping.Original.Color.ID, isPrint)
 			}
 			if mode&ALTERNATES != 0 {
-				mapPart(mapping, providedCollection, mapping.Original.Color.ID, isAlternative)
+				mapPart(providedCollection, mapping, mapping.Original.Color.ID, isAlternative)
 			}
 		}
 	}
 }
 
-func mapEquivalentShapeDifferentColor(mode uint8, buildCollection model.BuildCollection, colors *model.Colors, providedCollection *model.Collection,
-	isMold func(s1 string, s2 string) bool, isPrint func(s1 string, s2 string) bool, isAlternative func(s1 string, s2 string) bool) {
-
+func mapEquivalentShapeDifferentColor(providedCollection *model.Collection, buildCollection *model.BuildCollection, mode uint8) {
 	if mode&COLOR != 0 {
+		colors := model.GetColors(false)
+
 		for i := range buildCollection.Mapping {
 			mapping := &buildCollection.Mapping[i]
 			for j := 0; j < len(colors.Colors) && mapping.Quantity > 0; j++ {
 				colorId := colors.Colors[j].ID
 				if mapping.Original.Color.ID != colorId {
-					mapPart(mapping, providedCollection, colorId, utils.Equals)
+					mapPart(providedCollection, mapping, colorId, utils.Equals)
 					if mode&MOLDS != 0 {
-						mapPart(mapping, providedCollection, colorId, isMold)
+						mapPart(providedCollection, mapping, colorId, isMold)
 					}
 					if mode&PRINTS != 0 {
-						mapPart(mapping, providedCollection, colorId, isPrint)
+						mapPart(providedCollection, mapping, colorId, isPrint)
 					}
 					if mode&ALTERNATES != 0 {
-						mapPart(mapping, providedCollection, colorId, isAlternative)
+						mapPart(providedCollection, mapping, colorId, isAlternative)
 					}
 				}
 			}
@@ -121,9 +97,7 @@ func mapEquivalentShapeDifferentColor(mode uint8, buildCollection model.BuildCol
 	}
 }
 
-func mapPart(mapping *model.PartMapping, providedCollection *model.Collection,
-	colorId int, eqFunc func(string, string) bool) {
-
+func mapPart(providedCollection *model.Collection, mapping *model.PartMapping, colorId int, eqFunc func(string, string) bool) {
 	if mapping.Quantity > 0 {
 		providedPart := providedCollection.FindByEquivalence(
 			mapping.Original.Shape.Number, colorId, eqFunc)
@@ -144,4 +118,16 @@ func mapPart(mapping *model.PartMapping, providedCollection *model.Collection,
 				providedPart.Color.ID, providedPart.Quantity)
 		}
 	}
+}
+
+func isMold(s1, s2 string) bool {
+	return model.GetPartRelationships(false).IsMold(s1, s2)
+}
+
+func isPrint(s1, s2 string) bool {
+	return model.GetPartRelationships(false).IsPrint(s1, s2)
+}
+
+func isAlternative(s1, s2 string) bool {
+	return model.GetPartRelationships(false).IsAlternative(s1, s2)
 }

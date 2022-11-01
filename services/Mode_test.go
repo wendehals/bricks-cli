0140ae7928c0fddae5ca5f213449f3316dc543ca
Lothar Wendehals <lothar.wendehals@itemis.de>
2022-11-01 16:26:49 +0100

    added mode_test

---
+++ services/Mode_test.go
@@ -0,0 +1,35 @@
+package services
+
+import (
+	"testing"
+
+	"github.com/wendehals/bricks/test"
+)
+
+func Test_ColorMode(t *testing.T) {
+	test.AssertTrue(t, ColorMode(ModeToUInt8("c")))
+	test.AssertFalse(t, ColorMode(ModeToUInt8("amp")))
+}
+
+func Test_PrintsMode(t *testing.T) {
+	test.AssertTrue(t, PrintsMode(ModeToUInt8("camp")))
+	test.AssertFalse(t, PrintsMode(ModeToUInt8("cam")))
+}
+
+func Test_MoldsMode(t *testing.T) {
+	test.AssertTrue(t, MoldsMode(ModeToUInt8("camp")))
+	test.AssertFalse(t, MoldsMode(ModeToUInt8("c")))
+}
+
+func Test_AlternatesMode(t *testing.T) {
+	test.AssertTrue(t, AlternatesMode(ModeToUInt8("camp")))
+	test.AssertFalse(t, AlternatesMode(ModeToUInt8("cmp")))
+}
+
+func Test_ModeToUInt8(t *testing.T) {
+	test.AssertSameInt(t, 1, int(ModeToUInt8("c")))
+	test.AssertSameInt(t, 2, int(ModeToUInt8("a")))
+	test.AssertSameInt(t, 4, int(ModeToUInt8("m")))
+	test.AssertSameInt(t, 8, int(ModeToUInt8("p")))
+	test.AssertSameInt(t, 15, int(ModeToUInt8("camp")))
+}
